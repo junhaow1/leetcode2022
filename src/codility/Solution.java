9ef5f309a3465652fa4a1d15d4eae2c070a0ef5f
kev <u7274552@anu.edu.au>
2022-10-17 23:47:41 +1100

    Q2 done

--- src/codility/Solution.java
+++ src/codility/Solution.java
@@ -9,33 +9,48 @@ package codility;
 class Solution {
     public int solution(String S) {
         // write your code in Java 8
-        if(S.length() < 2) return -1;
+        if (S.length() < 2) return -1;
         int count = 0;
-        for (int i = 0; i < S.length(); i++){
+        for (int i = 0; i < S.length(); i++) {
             boolean flag = true;
-            if ('a'<=S.charAt(i) && S.charAt(i)<='z'){
-                char in = (char)(S.charAt(i)-32);
-                for (int j = 0; j < S.length(); j++){
-                    if (in == S.charAt(j)){
+            if ('a' <= S.charAt(i) && S.charAt(i) <= 'z') {
+                char in = (char) (S.charAt(i) - 32);
+                for (int j = 0; j < S.length(); j++) {
+                    if (in == S.charAt(j)) {
                         count++;
                         flag = false;
                         break;
                     }
                 }
-            }
-            if ('A'<=S.charAt(i) && S.charAt(i)<='Z'){
-                char in = (char)(S.charAt(i)+32);
-                for (int j = 0; j < S.length(); j++){
-                    if (in == S.charAt(j)){
+            } else if ('A' <= S.charAt(i) && S.charAt(i) <= 'Z') {
+                char in = (char) (S.charAt(i) + 32);
+                for (int j = 0; j < S.length(); j++) {
+                    if (in == S.charAt(j)) {
                         count++;
                         flag = false;
                         break;
                     }
                 }
+            }else{
+                break;
             }
             if (flag) break;
         }
         if (count < 2) return -1;
         return count;
+    }
+
+    public String solution(String message, int K) {
+        // write your code in Java SE 11
+        if (K >= message.length()) return message;
+        String[] split = message.split("\\s+");
+        String str = "";
+        if (split[0].length() > K) return "";
+        str = split[0];
+        for (int i = 1; i < split.length; i++){
+            if ((str + " " + split[i]).length() > K) return str;
+            else str += " " + split[i];
+        }
+        return str;
     }
 }
